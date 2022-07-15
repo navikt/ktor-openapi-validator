375a2f07df075e43579098c5fb254ed0ec7a90f1
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-07-15 11:22:42 +0200

    wip methodAssertion: tests

--- src/test/kotlin/no/nav/openapi/OpenapiValidatorTest.kt
+++ src/test/kotlin/no/nav/openapi/OpenapiValidatorTest.kt
@@ -5,6 +5,7 @@ import io.ktor.server.routing.Routing
 import no.nav.openapi.ApplicationSpec.Companion.routesInApplication
 import no.nav.openapi.PathAssertion.OpenApiPathError
 import no.nav.openapi.utils.SimpleTestRoute
+import no.nav.openapi.utils.userDir
 import no.nav.openapi.utils.withSimpleRoute
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
@@ -16,21 +17,22 @@ class OpenapiValidatorTest {
     @Test
     fun `happy path`() {
         withSimpleRoute {
-            val application = plugin(Routing).routesInApplication()
+            val applicationRoutes = plugin(Routing).routesInApplication()
             val openApiSpec = OpenApiSpec.fromJson(SimpleTestRoute.correctSpecFile)
             assertDoesNotThrow {
-                openApiSpec `should contain the same paths as` application
+                openApiSpec `should contain the same paths as` applicationRoutes
+                openApiSpec `paths should have the same methods as` applicationRoutes
             }
         }
     }
 
     @Test
-    fun `errors in openapi spec`() {
+    fun `path errors in openapi spec`() {
         withSimpleRoute {
-            val application = plugin(Routing).routesInApplication()
-            val openApiSpec = OpenApiSpec.fromJson(SimpleTestRoute.spechFilePathWith1MissingAnd2superflous)
+            val applicationRoutes = plugin(Routing).routesInApplication()
+            val openApiSpec = OpenApiSpec.fromJson(SimpleTestRoute.`specfile with 2 superflous and 1 missing path `)
             assertThrows<OpenApiPathError> {
-                openApiSpec `should contain the same paths as` application
+                openApiSpec `should contain the same paths as` applicationRoutes
             }.also {
                 assertEquals(1, it.missing, "Wrong missing count")
                 assertEquals(2, it.superflous, "Wrong superflous count")
@@ -39,20 +41,44 @@ class OpenapiValidatorTest {
     }
 
     @Test
-    fun withRecovery() {
+    fun `method errors in openapi spec`(){
         withSimpleRoute {
-            val application = plugin(Routing).routesInApplication()
-            val correctOpenApiSpec = OpenApiSpec.fromJson(SimpleTestRoute.correctSpecFile)
-            val openApiSpecWithErrors = OpenApiSpec.fromJson(SimpleTestRoute.spechFilePathWith1MissingAnd2superflous)
-            withRecovery(SpecAssertionRecovery(correctOpenApiSpec, application)) {
-                assertDoesNotThrow { correctOpenApiSpec `should contain the same paths as` application }
+            val applicationRoutes = plugin(Routing).routesInApplication()
+            val openApiSpec = OpenApiSpec.fromJson(SimpleTestRoute.`specfile with 3 missing and 2 superflous methods`)
+            assertThrows<PathAssertion.OpenApiMethodError> {
+                openApiSpec `should contain the same paths as` applicationRoutes
+            }.also {
+                assertEquals(3,it.missing)
+                assertEquals(2, it.missing)
+
+                assertEquals(1, it.inPath("/simple/test").missing)
+                assertEquals(2, it.inPath("/simple/test").superflous)
+                assertEquals(2, it.inPath("/simple/test/{id}").missing)
+                assertEquals(0, it.inPath("/simple/test/{id}").superflous)
             }
-            assertThrows<MissingSpecContentError> {
-                withRecovery(SpecAssertionRecovery(openApiSpecWithErrors, application)) {
-                    openApiSpecWithErrors `should contain the same paths as` application
+
+        }
+    }
+
+
+    @Test
+    fun `performs recovery and generates correct paths`() {
+        withSimpleRoute {
+            val recoveryfilePath = "$userDir/build/tmp/openapi.json"
+            val applicationRoutes = plugin(Routing).routesInApplication()
+            val correctOpenApiSpec = OpenApiSpec.fromJson(SimpleTestRoute.correctSpecFile)
+            val openApiSpecWithErrors = OpenApiSpec.fromJson(SimpleTestRoute.`specfile with 2 superflous and 1 missing path `)
+            assertDoesNotThrow {
+                withRecovery(SpecAssertionRecovery(correctOpenApiSpec, applicationRoutes)) {
+                    correctOpenApiSpec `should contain the same paths as` applicationRoutes
                 }
             }
-            //TODO: Legg til missing/superflous
+            assertThrows<MissingSpecContentError> {
+                withRecovery(SpecAssertionRecovery(openApiSpecWithErrors, applicationRoutes,recoveryfilePath)) {
+                    openApiSpecWithErrors `should contain the same paths as` applicationRoutes
+                }
+            }
+            assertDoesNotThrow { OpenApiSpec.fromJson(recoveryfilePath) `should contain the same paths as` applicationRoutes }
         }
     }
 }
--- src/test/kotlin/no/nav/openapi/utils/TestRoutes.kt
+++ src/test/kotlin/no/nav/openapi/utils/TestRoutes.kt
@@ -1,28 +1,39 @@
 package no.nav.openapi.utils
 
 import io.ktor.server.application.Application
+import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
+import io.ktor.server.routing.put
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import no.nav.openapi.Method
 import no.nav.openapi.Path
 import no.nav.openapi.utils.SimpleTestRoute.simpleTestRoute
 
-private val userDir = System.getProperty("user.dir")
-internal class TestPath(pathString: String, methods: List<Method>) :Path(pathString, methods)
+internal val userDir = System.getProperty("user.dir")
+
+internal class TestPath(pathString: String, methods: List<Method>) : Path(pathString, methods)
 internal object SimpleTestRoute {
-    val spechFilePathWith1MissingAnd2superflous: String ="$userDir/src/test/resources/openapi/simpleapi-error-in-paths.json"
+    val `specfile with 2 superflous and 1 missing path ` =
+        "$userDir/src/test/resources/openapi/simpleapi-error-in-paths.json"
+    val `specfile with 3 missing and 2 superflous methods` =
+        "$userDir/src/test/resources/openapi/simpleapi-error-in-methods.json"
     internal val testPath = "/test/simple"
     internal val correctSpecFile = "$userDir/src/test/resources/openapi/simpleapi.json"
     internal val expectedPaths = listOf(
-        TestPath(pathString = testPath, methods = listOf(
-            Method("get"), Method("post")
-        )),
-        TestPath(pathString = "$testPath/{id}", methods = listOf(
-            Method("get")
-        ))
+        TestPath(
+            pathString = testPath, methods = listOf(
+                Method("get"), Method("post")
+            )
+        ),
+        TestPath(
+            pathString = "$testPath/{id}", methods = listOf(
+                Method("get")
+            )
+        )
     )
+
     internal fun Application.simpleTestRoute() {
         routing {
             route(testPath) {
@@ -31,6 +42,9 @@ internal object SimpleTestRoute {
             }
             route("$testPath/{id}") {
                 get { }
+                post { }
+                delete { }
+                put { }
             }
 
         }
